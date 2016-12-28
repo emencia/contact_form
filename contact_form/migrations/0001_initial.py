0cd48990e563b44eda39967cfdf56654315d8609
Adrien Brunet <adrien@emencia.com>
2016-12-28 13:37:39 +0100

    :bug: Fix migrations to support older versions of django

--- contact_form/migrations/0001_initial.py
+++ contact_form/migrations/0001_initial.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('sites', '0002_alter_domain_unique'),
+        ('sites', '0001_initial'),
     ]
 
     operations = [
